49aa8c212e7353a3e71a0c3769e29d14e9feafbf
Jun Sasaki <jsasaki.ece@gmail.com>
2025-01-27 15:36:53 +0900

    Initial commit

---
+++ examples/create_gif_anim_2d_from_frames.py
@@ -0,0 +1,6 @@
+from xfvcom.helpers import create_gif_from_frames
+
+frames_dir = "./frames"
+output_gif = "salinity_2020_surface.gif"
+
+create_gif_from_frames(frames_dir=frames_dir, output_gif=output_gif, fps=10, batch_size=200, cleanup=False)
