46d6534e17c0f2110d807103a0f0e61f021076f9
Jun Sasaki <jsasaki.ece@gmail.com>
2025-05-01 16:45:59 +0900

    Minor change

--- tests/test_plot_config.py
+++ tests/test_plot_config.py
@@ -2,5 +2,5 @@ from xfvcom.plot.config import FvcomPlotConfig
 
 def test_dataclass_defaults():
     cfg = FvcomPlotConfig()
-    assert cfg.figsize == (8.0, 6.0)
+    assert cfg.figsize == (8.0, 2.0)
     assert cfg._private_cache == {}
