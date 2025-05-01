2cd5a5c6100639c1caf0fa7f7e0ad909c53f8087
Jun Sasaki <jsasaki.ece@gmail.com>
2025-05-01 14:51:08 +0900

    feat(bc): add shim module xfvcom.decorators

---
+++ xfvcom/decorators.py
@@ -0,0 +1,6 @@
+"""
+shim module (2025-05):
+`xfvcom.decorators` was moved to `xfvcom.utils.decorators`.
+This file re-exports everything for backward compatibility.
+"""
+from .utils.decorators import *   # noqa: F401,F403
