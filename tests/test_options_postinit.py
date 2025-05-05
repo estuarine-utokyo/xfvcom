d18174c224e02f59fc7560cd6d2abbce06141a74
Jun Sasaki <jsasaki.ece@gmail.com>
2025-05-05 22:51:25 +0900

    Initial commit

---
+++ tests/test_options_postinit.py
@@ -0,0 +1,9 @@
+# tests/test_options_postinit.py
+from xfvcom.plot.plot_options import FvcomPlotOptions
+
+
+def test_post_init_normalization():
+    opts = FvcomPlotOptions(arrow_scale="auto")
+    assert opts.arrow_scale is None
+    assert isinstance(opts.vec_reduce, dict) and opts.vec_reduce == {}
+    assert isinstance(opts.scalar_reduce, dict)
