9865a93aedc5aada07e81eb278be6f9ed4fcec03
Jun Sasaki <jsasaki.ece@gmail.com>
2025-09-06 11:24:42 +0900

    Cleaned

--- xfvcom/plot/__init__.py
+++ xfvcom/plot/__init__.py
@@ -11,6 +11,18 @@ from .config import FvcomPlotConfig
 from .core import FvcomPlotter  # ← 追加すると便利
 from .markers import make_node_marker_post
 
+# Plotly utilities (optional, only if plotly is installed)
+try:
+    from .plotly_utils import (
+        plot_timeseries_comparison,
+        plot_timeseries_multi_variable,
+        create_river_extension_plot,
+        print_plotly_instructions,
+    )
+    PLOTLY_AVAILABLE = True
+except ImportError:
+    PLOTLY_AVAILABLE = False
+
 # ------------------------------------------------------------------
 # Public symbol table
 # ------------------------------------------------------------------
@@ -19,3 +31,12 @@ __all__: list[str] = [
     "FvcomPlotter",
     "make_node_marker_post",
 ]
+
+# Add plotly functions to __all__ if available
+if PLOTLY_AVAILABLE:
+    __all__.extend([
+        "plot_timeseries_comparison",
+        "plot_timeseries_multi_variable", 
+        "create_river_extension_plot",
+        "print_plotly_instructions",
+    ])
