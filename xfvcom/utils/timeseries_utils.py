206d9721efc35b4ecc2363267bff0bc5e536a1d7
Jun Sasaki <jsasaki.ece@gmail.com>
2025-09-06 12:04:13 +0900

    Fixed GitHub tests issue

--- xfvcom/utils/timeseries_utils.py
+++ xfvcom/utils/timeseries_utils.py
@@ -223,7 +223,7 @@ def extend_timeseries_seasonal(
         pattern_data = df.copy()
     else:
         # Extract the pattern from the last complete period
-        pattern_data = df.loc[last_period_start : df.index[-1]].copy()
+        pattern_data = df.loc[last_period_start:df.index[-1]].copy()  # fmt: skip
 
     # Create result DataFrame
     result = df.reindex(extended_index)
